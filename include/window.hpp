e1f601358967ee847f9d2526f35787c2c8d5b8b1
Max <maxoblack@yahoo.com>
2021-02-22 18:17:32 +0000

    Have Window destructor destroy just that window instead of terminate GLFW entirely

--- include/window.hpp
+++ include/window.hpp
@@ -2,13 +2,13 @@
 
 #include <GLFW/glfw3.h>
 
-/// Wrapper around a `GLFWwindow` object. Automatically calls `glfwTerminate` on destruction.
+/// Wrapper around a `GLFWwindow` object. Automatically calls `glfwDestroyWindow` on destruction.
 class Window {
 public:
     /// Creates a window with the specified dimensions and title. Will throw `std::runtime_error` should GLFW fail to
     /// create the window.
     void create(int width, int height, const char* title);
-    GLFWwindow* operator*();
+    GLFWwindow* operator*() const;
     ~Window();
 
 private:
--- src/window.cpp
+++ src/window.cpp
@@ -1,21 +1,25 @@
 #include "window.hpp"
 
 #include <stdexcept>
+#include <iostream>
 
 void Window::create(int width, int height, const char* title) {
     handle = glfwCreateWindow(width, height, title, nullptr, nullptr);
+
     if(handle == nullptr) {
-        glfwTerminate();
         throw std::runtime_error("Failed to create GLFW window");
     }
 
     glfwMakeContextCurrent(handle);
+
+    std::cout << "Window created" << std::endl;
 }
 
-GLFWwindow* Window::operator*() {
+GLFWwindow* Window::operator*() const {
     return handle;
 }
 
 Window::~Window() {
-    glfwTerminate();
+    glfwDestroyWindow(handle);
+    std::cout << "Window destroyed" << std::endl;
 }
