7fad3b7f7c840c38a50187cde702502db1ffb676
Max <maxoblack@yahoo.com>
2021-02-24 12:39:04 +0000

    Fix order of initialisation in FPS class

--- src/fps.cpp
+++ src/fps.cpp
@@ -1,21 +1,21 @@
 #include "fps.hpp"
 
-FramesPerSecond::FramesPerSecond(double rate) : rate(rate), fps(0.0), time_since_calc(0.0), updates_since_calc(0) {}
+FramesPerSecond::FramesPerSecond(double rate) : fps(0.0), time_since_calc(0.0), updates_since_calc(0), rate(rate) {}
 
 bool FramesPerSecond::update(double delta) {
     updates_since_calc++;
     time_since_calc += delta;
 
-    if(time_since_calc >= rate) {
+    bool will_recalculate = time_since_calc >= rate;
+
+    if(will_recalculate) {
         fps = static_cast<double>(updates_since_calc) / time_since_calc;
 
         updates_since_calc = 0;
         time_since_calc = 0.0;
-
-        return true;
     }
 
-    return false;
+    return will_recalculate;
 }
 
 double FramesPerSecond::get() const {
